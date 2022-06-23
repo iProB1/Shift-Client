4b5c2ee1e7520393ab8a9f3cb1ef12c6757dbcc3
ProB1 <50064486+iProB1@users.noreply.github.com>
2022-06-23 22:08:57 +0300

    Directx9 imgui hook + TextEditor + Enet
    you guys can add them ur self too but i just share base 
    so have fun

---
+++ Shift/main.cpp
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <iostream>
+#include <fstream>
+#include <windows.h>
+#include "imgui/TextEditor.h" // there text editor you can use it for executor
+
+void Tab::Main(){
+		ImGuiTabBarFlags tbflags = ImGuiTabBarFlags_None;
+		if (ImGui::BeginTabBar("MyTabBar", tbflags))
+		{
+			if (ImGui::BeginTabItem("Cheats"))
+			{
+				ImGui::Text("its base of Shift client\nMade By Shift#0100 (old nick ProB1#0100 )");
+				ImGui::EndTabItem();
+			}
+			ImGui::EndTabBar();
+		}
+}
+void Tab::Another() {
+   // add another menus thats why here
+}
---
+++ Shift/main.h
@@ -0,0 +1,10 @@
+#pragma once
+//#include <include/enet.h>
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_dx9.h"
+#include "imgui/imgui_impl_win32.h"
+
+namespace Tab {
+	void Main();
+	void Another();
+}
